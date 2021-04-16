bb68c5fe9065def087d7ef2336c8135307b3f41f
alejandro medici <el.aleyo@gmail.com>
2021-04-15 21:31:55 -0300

    Borro lista de mi carrito

--- js/events.js
+++ js/events.js
@@ -10,10 +10,19 @@ function myButtonClicked(parametro) {
 // Con Event Listener
 //myButtonLogin.addEventListener("click", myButtonClicked);
 
+// Asigne a todos los botones la misma funcion
 for (const button of myButtonsLogin) {
     button.addEventListener("click", myButtonClicked);
 }
 
+
+let limpiar = document.getElementById("clean");
+
+limpiar.onclick = () => {
+    let lista = document.getElementById("myCarrito");
+    lista.parentNode.removeChild(lista)
+    console.log("Voy a limpiar el carrito")
+};
 
 
 // Ahora Arroy function
